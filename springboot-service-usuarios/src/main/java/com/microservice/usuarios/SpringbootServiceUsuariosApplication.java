b02b5cf08892f467cae53361d946e892b84aa983
Monserrat Fernández <xiomistar2003@hotmail.com>
2024-04-24 12:40:40 -0600

    Tarea 5

---
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/SpringbootServiceUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceUsuariosApplication.class, args);
+	}
+
+}
